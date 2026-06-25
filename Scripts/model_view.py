5562a41b9138ffb4616be0cccc8037e590c2b04b
huanglili <aa483680845@email.com>
2026-06-26 00:53:02 +0800

    修复了机械臂第一个关节阻尼问题

--- Scripts/model_view.py
+++ Scripts/model_view.py
@@ -9,6 +9,12 @@ model = mujoco.MjModel.from_xml_path(str(MODEL_PATH))
 data = mujoco.MjData(model)
 
 with mujoco.viewer.launch_passive(model, data) as v:
+    # 添加mujoco GUI默认启动配置
+    v.opt.flags[mujoco.mjtVisFlag.mjVIS_TRANSPARENT] = True
+    v.opt.flags[mujoco.mjtVisFlag.mjVIS_COM] = True
+    v.opt.flags[mujoco.mjtVisFlag.mjVIS_JOINT] = True
+    v.opt.frame = mujoco.mjtFrame.mjFRAME_BODY
+
     real_start = time.time()
     while v.is_running():
         mujoco.mj_step(model, data)
